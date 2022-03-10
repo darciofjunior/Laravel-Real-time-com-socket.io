b9268e87cfc4dcc4ffd76ba5b3e17089f6e104a2
darciofjunior <darciofjunior81@gmail.com>
2022-03-10 18:14:40 -0300

    Curso de Laravel Real-time com socket.io

---
+++ resources/js/Echo.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Bus from './bus'
+
+window.Echo.channel('laravel_database_post-created')
+            .listen('PostCreated', (e) => {
+                Bus.$emit('post.created', e.post)
+
+                Vue.$vToastify.success(`Título do post ${e.post.name}`, 'Novo Post')
+            })
